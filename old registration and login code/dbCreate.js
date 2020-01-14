6565fbd8a8cb8cdaef45d5ba63ab74abac11efdd
DobeZz <46678471+DobeZz@users.noreply.github.com>
2020-01-14 21:48:04 +0000

    Add dbCreate.js - file to create the users database

---
+++ old registration and login code/dbCreate.js
@@ -0,0 +1,10 @@
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database('./cipherMail.db')
+
+db.serialize(() => {
+db.run("CREATE TABLE accounts (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username TEXT, password TEXT)");
+});
+db.close();
+
+console.log('database initialsed');
+
