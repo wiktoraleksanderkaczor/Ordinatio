11d0b49b35f0461cfe1571519591c20c6139c7e3
DobeZz <46678471+DobeZz@users.noreply.github.com>
2020-01-15 10:05:33 +0000

    Update dbCreate.js
    fixed name of the database file

--- old registration and login code/dbCreate.js
+++ old registration and login code/dbCreate.js
@@ -1,5 +1,5 @@
 const sqlite3 = require('sqlite3');
-const db = new sqlite3.Database('./cipherMail.db')
+const db = new sqlite3.Database('./users.db')
 
 db.serialize(() => {
 db.run("CREATE TABLE accounts (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username TEXT, password TEXT)");
